dee0d8e10c9b8b853a6982b3b35cd9e0c0eef25b
YuhangSuen <18801300138@163.com>
2025-07-31 18:31:28 +0800

    ENGLISH

--- src/solution/mapping_utils.py
+++ src/solution/mapping_utils.py
@@ -10,34 +10,34 @@ This module provides the mapping relationship between liquid and gas species, us
 """
 # 液相组分名称字典
 LIQUID_SPECIES_NAMES = {
-    # NA系列 (0-9)
+    # NA hydrocarbon family (0-9)
     0: "NA7", 1: "NA8", 2: "NA9", 3: "NA10", 4: "NA11",
     5: "NA12", 6: "NA13", 7: "NA14", 8: "NA15", 9: "NA16",
-    # IA系列 (10-19)
+    # IA hydrocarbon family (10-19)
     10: "IA7", 11: "IA8", 12: "IA9", 13: "IA10", 14: "IA11",
     15: "IA12", 16: "IA13", 17: "IA14", 18: "IA15", 19: "IA16",
-    # CA系列 (20-29)
+    # CA hydrocarbon family (20-29)
     20: "CA7", 21: "CA8", 22: "CA9", 23: "CA10", 24: "CA11",
     25: "CA12", 26: "CA13", 27: "CA14", 28: "CA15", 29: "CA16",
-    # AR系列 (30-39)
+    # AR hydrocarbon family (30-39)
     30: "AR7", 31: "AR8", 32: "AR9", 33: "AR10", 34: "AR11",
     35: "AR12", 36: "AR13", 37: "AR14", 38: "AR15", 39: "AR16"
 }
 
 # 气相组分名称字典
 GAS_liquid_dict = {
-    # NA系列对应的NC系列
+    # NA hydrocarbon family对应的NC系列
     "NA7": "NC7H16", "NA8": "NC8H18", "NA9": "NC9H20", "NA10": "NC10H22", "NA11": "NC11H24",
     "NA12": "NC12H26", "NA13": "NC13H28", "NA14": "NC14H30", "NA15": "NC15H32", "NA16": "NC16H34",
     
-    # IA系列对应的C系列-2
+    # IA hydrocarbon family对应的C系列-2
     "IA7": "C7H16-2", "IA8": "C8H18-2", "IA9": "C9H20-2", "IA10": "C10H22-2", "IA11": "C11H24-2",
     "IA12": "C12H26-2", "IA13": "C13H28-2", "IA14": "C14H30-2", "IA15": "C15H32-2", "IA16": "C16H34-2",
     
-    # CA系列对应的CH3cC6H11系列
+    # CA hydrocarbon family对应的CH3cC6H11系列
     "CA7": "CH3cC6H11",  "CA8": "C2H5cC6H11", "CA9": "C3H7cC6H11", "CA10": "C4H9cC6H11", "CA11": "C5H11cC6H11",
     
-    # AR系列对应关系
+    # AR hydrocarbon family对应关系
     "AR7": "C6H5CH3",  "AR8": "A1C2H5", "AR9": "A1C3H7", "AR10": "A1C4H9", "AR11": "A1C5H11", "AR12": "A1C6H13"
 }
 
